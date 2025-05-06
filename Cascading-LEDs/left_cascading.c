a8d3d90436eedff84c2dd8bbe446e72aea5386ae
KIARA <kiarazhu386@gmail.com>
2025-05-06 16:31:41 +1000

    Update left_cascading.c

--- Cascading-LEDs/left_cascading.c
+++ Cascading-LEDs/left_cascading.c
@@ -7,20 +7,21 @@ int main(void) {
 	// Set port A pins to be outputs
 	DDRA = 0xFF;
 	
-	// Begin with all LEDs on
-	uint8_t led = 0xFF;
+	// Begin with all LEDs off
+	uint8_t led = 0x00;
 
 	while(1) {
-		PORTA = led ^ 0xFF;    // Display the led pattern
-		_delay_ms(100);
-		led = (led >> 1);     // Update the LED value
 		
-		/* Once the end of the pattern is reached, illuminate
-		all leds before restarting the pattern */
-        if (led == 0x00) {
-			PORTA = led ^ 0xFF;    // Display the led pattern
-			_delay_ms(100);
-            led = 0xFF;
-		}
+	    PORTA = led;    // Display the led pattern
+	    _delay_ms(100);
+	    led = (led >> 1) + 0x80;     // Update the LED value
+		
+	/* Once the rightmost LED is reached, illuminate
+	all leds before restarting the pattern */
+        if (led == 0xFF) {
+	    PORTA = led;    // Display the led pattern
+    	    _delay_ms(100);
+            led = 0x00;
 	}
+    }
 }
